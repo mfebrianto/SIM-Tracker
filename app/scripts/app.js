bfbde8ba15d894baea556207d0e492bc263f203d
mfebrianto <michaelfebrianto@gmail.com>
2015-10-19 12:28:54 +1100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name computerVisionApp
+ * @description
+ * # computerVisionApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('computerVisionApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'camera'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name computerVisionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the computerVisionApp
+ */
+angular.module('computerVisionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
